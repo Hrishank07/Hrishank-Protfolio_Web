e623878922df2707f898184f49ce55a810e18f2f
Hrishank07 <hrushank.cc@gmail.com>
2025-03-11 18:59:35 -0700

    Updated the files for netlify

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BiD7vo0qgeiQT8pD/zko7HNjAXOOW/pwzfmCCvH7Bns=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
